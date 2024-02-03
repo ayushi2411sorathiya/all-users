63956a666614d00b32f2c934b64602abe5e79631
ayushi2411sorathiya <ayushisorathiya11@gmail.com>
2024-02-03 11:25:53 +0530

    Updates

--- sw.js
+++ sw.js
@@ -9,10 +9,55 @@ const urlCache = [
     "/sw.js",
     "/manifest.json",
     "/logo192.png",
-    "/users"
+    "/users",
+    "/all-users",
+    "/all-users/users",
+    "/all-users/static/js/bundle.js",
+    "/all-users/favicon.ico",
+    "/all-users/manifest.json",
+    "/all-users/logo192.png"
 ]
 
-this.addEventListener("install", (event) => {
+// this.addEventListener("install", (event) => {
+//     event.waitUntil(
+//         caches.open(CACHE_NAME)
+//             .then((cache) => {
+//                 console.log("open cache");
+//                 return cache.addAll(urlCache)
+//             })
+//     )
+// })
+
+// this.addEventListener("fetch", (event) => {
+//     if (!navigator.onLine) {
+//         event.respondWith(
+//             caches.match(event.request)
+//                 .then((response) => {
+//                     if (response) {
+//                         return response
+//                     }
+//                     let furl = event.request.clone()
+//                     fetch(furl)
+//                 })
+//         )
+//     }
+// })
+
+// self.addEventListener("activate", (event) => {
+//     const cacheWhiteList = [];
+//     cacheWhiteList.push(CACHE_NAME);
+
+//     event.waitUntil(
+//         caches.keys().then((cacheName) => Promise.all(
+//             cacheName.map((cacheName) => {
+//                 if (!cacheWhiteList.includes(cacheName))
+//                     return caches.delete(cacheName)
+//             })
+//         ))
+//     )
+// })
+
+self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
@@ -22,33 +67,31 @@ this.addEventListener("install", (event) => {
     )
 })
 
-this.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", (event) => {
     if (!navigator.onLine) {
         event.respondWith(
             caches.match(event.request)
                 .then((response) => {
                     if (response) {
-                        return response
+                        return response;
                     }
-                    let furl = event.request.clone()
-                    fetch(furl)
+                    let furl = event.request.clone();
+                    return fetch(furl);
                 })
         )
     }
 })
 
 self.addEventListener("activate", (event) => {
-    const cacheWhiteList = [];
-    cacheWhiteList.push(CACHE_NAME);
+    const cacheWhiteList = [CACHE_NAME];
 
     event.waitUntil(
-        caches.keys().then((cacheName) => Promise.all(
-            cacheName.map((cacheName) => {
-                if (!cacheWhiteList.includes(cacheName))
-                    return caches.delete(cacheName)
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhiteList.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
             })
         ))
     )
 })
-
-
